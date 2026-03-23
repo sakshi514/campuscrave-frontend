0ef4b5ffcdd1657e3639ffe560f356f1332830aa
Sakshi Jain <sakshijainn514@gmail.com>
2026-03-23 23:48:11 +0530

    fix socket transport

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,3 @@
 import { io } from "socket.io-client";
-const socket = io("https://campuscrave-backend-gdrw.onrender.com", {
-    transports: ["websocket"],
-});
+const socket = io("https://campuscrave-backend-gdrw.onrender.com");
 export default socket;
