73f8cc7032f20e8807b21fdc62d04460a6bed631
Sakshi Jain <sakshijainn514@gmail.com>
2026-03-23 12:02:13 +0530

    updated API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://campuscrave-backend-gdrw.onrender.com",
 });
 
 // Attach token automatically
